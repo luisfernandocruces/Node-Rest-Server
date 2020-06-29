e9135f910884161cf6f442eae39491ee8a5a36da
luisfcv97 <luisfcv97@gmail.com>
2020-06-29 12:59:39 -0500

    updated enviorement variables

--- server/config/config.js
+++ server/config/config.js
@@ -7,3 +7,21 @@
 // Port
 // ==========================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==========================================
+// Enviorement
+// ==========================================
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+// ==========================================
+// Data Base
+// ==========================================
+let urlDB;
+
+if (process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/coffe";
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require("./config/config");
 const express = require("express");
 const app = express();
+const colors = require("colors");
+
+const mongoose = require("mongoose");
 
 //This allow serializar all request in json object
 const bodyParser = require("body-parser");
@@ -16,32 +19,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/user", function(req, res) {
-    res.json("Get user");
-});
+app.use(require("./routes/user"));
 
-app.post("/user", function(req, res) {
-    let body = req.body;
-    if (body.name === undefined) {
-        res.status(400).json({
-            ok: false,
-            message: "Name is required",
-        });
-    } else {
-        res.json({ person: body });
+/**
+ * If db does not exists... mongo can connect and then, when you use that db,
+ * It will create the structure in db
+ */
+mongoose.connect(
+    process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log("data base online".green);
     }
-});
-
-app.put("/user/:id", function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id,
-    });
-});
-
-app.delete("/user", function(req, res) {
-    res.json("delete user");
-});
+);
 
 app.listen(process.env.PORT, () => {
     console.log("Listening server with port: ", 3000);
